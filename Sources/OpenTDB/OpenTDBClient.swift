b2aba7aea93240bce48f2eff79a8196f20215b3d
Kody Deda <kodydeda4@gmail.com>
2025-03-19 11:53:44 -0400

    Update OpenTDBClient.swift

--- Sources/OpenTDB/OpenTDBClient.swift
+++ Sources/OpenTDB/OpenTDBClient.swift
@@ -1,10 +1,14 @@
 import Foundation
 
 public struct OpenTDBClient: Sendable {
-  public static let shared = OpenTDBClient()
+  public var verbose = false
+  
+  public init(verbose: Bool = false) {
+    self.verbose = verbose
+  }
   
   @Sendable public func fetchAllCategories() async throws -> FetchAllCategoriesResponse {
-    try await OpenTDBClient.request(
+    try await request(
       "https://opentdb.com/api_category.php"
     )
   }
@@ -14,7 +18,7 @@ public struct OpenTDBClient: Sendable {
     categoryId: Int,
     type: QuestionType = .mulipleChoice
   ) async throws -> FetchQuestionsResponse {
-    try await OpenTDBClient.request(
+    try await request(
       "https://opentdb.com/api.php?amount=\(amount)&category=\(categoryId)&type=\(type.rawValue)"
     )
   }
@@ -98,23 +102,25 @@ extension OpenTDBClient.Category {
 }
 
 extension OpenTDBClient {
-  private static func request<T: Decodable>(_ urlString: String) async throws -> T {
+  private func request<T: Decodable>(_ urlString: String) async throws -> T {
     guard let url = URL(string: urlString) else {
       throw URLError(.badURL)
     }
     
     let (data, _) = try await URLSession.shared.data(from: url)
     
-    if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
-       let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted),
-       let jsonString = String(data: jsonData, encoding: .utf8) {
-      print("""
+    if verbose {
+      if let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+         let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted),
+         let jsonString = String(data: jsonData, encoding: .utf8) {
+        print("""
     ----------------------------------------------------------------------------------------------
     📡 JSON Response from: \(urlString)
     ----------------------------------------------------------------------------------------------
     \(jsonString)
     ----------------------------------------------------------------------------------------------
     """)
+      }
     }
     
     let jsonDecoder = JSONDecoder()
